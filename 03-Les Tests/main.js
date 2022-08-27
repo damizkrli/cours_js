943443cf654a3185e119f7a34bb638ed17c33cd3
damizkrli <karli.damien@gmail.com>
2022-08-27 16:24:40 +0200

    exo 4

--- 03-Les Tests/main.js
+++ 03-Les Tests/main.js
@@ -28,6 +28,37 @@ if (!sexe) { // Est-ce que je suis égale à false
 
 username = "matthieu";
 
-if (username.substr(0,1) === "M" || "m") {
+if (username.substr(0, 1) === "M" || "m") {
     console.log("Bravo");
+}
+
+// Exercice 4
+strengh = 2;
+
+if (strengh < 3) {
+    console.log("mage");
+} else if (strengh < 6) {
+    console.log("archer");
+} else {
+    console.log("guerrier");
+}
+
+// ou
+
+if (strengh > 5) {
+    console.log("guerrier");
+} else if(strengh > 2) {
+    console.log("archer");
+} else {
+    console.log("mage");
+}
+
+// ou
+
+if (strengh >= 6) {
+    console.log("guerrier");
+} else if(strengh >= 3) {
+    console.log("archer");
+} else {
+    console.log("mage");
 }
