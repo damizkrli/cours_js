1f125a7415b9b93114f47ece136fb58c7eafdb4b
damizkrli <karli.damien@gmail.com>
2022-08-27 16:08:15 +0200

    exo if

--- 03-Les Tests/main.js
+++ 03-Les Tests/main.js
@@ -2,9 +2,32 @@
 
 var anneeDeConstruction = 2022;
 
-if (anneeDeConstruction < 2000) {
+if (anneeDeConstruction <= 2000) {
     console.log("Voiture Polluante");
 } else {
     console.log("Voiture Conforme");
 }
 
+// Exercice 2
+
+var sexe = true;
+
+if (sexe) { // traduction = est-ce que je suis égale à true
+    console.log("homme");
+} else {
+    console.log("femme");
+}
+
+if (!sexe) { // Est-ce que je suis égale à false
+    console.log("femme");
+} else {
+    console.log("homme");
+}
+
+// Exercidce 3
+
+username = "matthieu";
+
+if (username.substr(0,1) === "M" || "m") {
+    console.log("Bravo");
+}
