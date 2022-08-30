19b9eab7a6c2f9ea993db15d2bb9b182bba506f1
damizkrli <karli.damien@gmail.com>
2022-08-30 20:41:52 +0200

    exo3

--- 05-Les Boucles/exo3.js
+++ 05-Les Boucles/exo3.js
@@ -1,3 +1,12 @@
-for (let i = 1; i <= 100; i + 2) {
-    console.log(i)
+// var result = 0;
+// for (let i = 0; i <= 200; i = i + 2) {
+//     result += i;
+// }
+
+// console.log("La somme des 100 premiers nombres est : " + result);
+var resultat = 0;
+for (let i = 0; i <= 200; i += 2) {
+    if (i % 2 === 0) {
+        console.log(resultat += i);
+    }
 }
